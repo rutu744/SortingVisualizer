8f4114f1803166ad33a2b946ba199b78cad62927
Shree <rutugaikwad744@gmail.com>
2025-04-08 13:34:06 +0530

    Initial commit with .gitignore

---
+++ import_algorithms.py
@@ -0,0 +1,13 @@
+
+from algorithms.BubbleSort import BubbleSort
+from algorithms.InsertionSort import InsertionSort
+from algorithms.MergeSort import MergeSort
+from algorithms.QuickSort import QuickSort
+from algorithms.HeapSort import HeapSort
+
+algorithms = {
+              "Bubble": BubbleSort(),
+              "Insertion": InsertionSort(),
+              "Merge": MergeSort(),
+              "Quick": QuickSort(),
+              "Heap": HeapSort(),}
